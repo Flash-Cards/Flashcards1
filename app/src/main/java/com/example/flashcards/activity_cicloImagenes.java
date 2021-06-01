23c478b4861d9144d24a9c4292c8f78cb80396c1
PedroMaldonado <pedrosms9@gmail.com>
2021-06-01 17:31:26 -0500

    Se agrego audio a la acitivdad de imagenes

--- app/src/main/java/com/example/flashcards/activity_cicloImagenes.java
+++ app/src/main/java/com/example/flashcards/activity_cicloImagenes.java
@@ -7,8 +7,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -16,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class activity_cicloImagenes extends AppCompatActivity {
@@ -25,12 +28,16 @@ public class activity_cicloImagenes extends AppCompatActivity {
     ImageView imagen;
     byte [] imageBytes;
     Button bpreguntas;
-    ArrayList<String> listapalabras = new ArrayList<>();
+    ArrayList<String> listapathsito = new ArrayList<>();
     final ArrayList<Bitmap> imagenes = new ArrayList<>();
     int carreglo;
-
+    String pathsito;
     Boolean repetir=true;
+    Boolean startCycle=true;
     //int carreglo=palabras.length;
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private MediaPlayer player = null;
 
     public int i=0;
 
@@ -49,7 +56,7 @@ public class activity_cicloImagenes extends AppCompatActivity {
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "Palabras", null, 2);
         SQLiteDatabase BaseDeDatabase = admin.getWritableDatabase();
         //consulta
-        Cursor fila = BaseDeDatabase.rawQuery("select imagen from palabras where seleccion = 1", null);
+        Cursor fila = BaseDeDatabase.rawQuery("select imagen, audio from palabras where seleccion = 1", null);
         Bundle extras = getIntent().getExtras();
 
         if (extras != null) {
@@ -63,6 +70,7 @@ public class activity_cicloImagenes extends AppCompatActivity {
                 imageBytes = fila.getBlob(0);
                 Bitmap objectBitmap = BitmapFactory.decodeByteArray(imageBytes,0, imageBytes.length);
                 imagenes.add(objectBitmap);
+                listapathsito.add(fila.getString(1));
             } while (fila.moveToNext());
             BaseDeDatabase.close();
             int nimagenes=imagenes.size();
@@ -74,8 +82,14 @@ public class activity_cicloImagenes extends AppCompatActivity {
         bpreguntas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startCycle = false;
+                player.stop();
+                player.reset();
+                player.release();
+                player = null;
                 Intent intent = new Intent(v.getContext(), PreguntasAudio.class);
                 startActivity(intent);
+                finish();
             }
         });
 
@@ -92,12 +106,23 @@ public class activity_cicloImagenes extends AppCompatActivity {
         }
     };
 
-    public void cicloImagenes(){
-        imagen.setImageBitmap(imagenes.get(i));
-        if(i>=carreglo) {
-            i = -1;
+    public void cicloImagenes() {
+        if (startCycle != false) {
+            imagen.setImageBitmap(imagenes.get(i));
+            pathsito = listapathsito.get(i);
+            player = new MediaPlayer();
+            try {
+                player.setDataSource(pathsito);
+                player.prepare();
+                player.start();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "prepare() failed");
+            }
+            if (i >= carreglo) {
+                i = -1;
+            }
+            mhandler.postDelayed(cambiar, mTiempoEnMilis2);
         }
-        mhandler.postDelayed(cambiar,mTiempoEnMilis2);
     }
 
     public void establecerTiempo(long milisegundos) {
